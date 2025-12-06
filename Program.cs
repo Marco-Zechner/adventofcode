32795c74f2c9c9ddc116f156369b0379f8cf36ba
Marco-Zechner <marcoflorian00956@gmail.com>
2025-12-06 01:55:33 +0100

    solved

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllText("../../../data.txt");
+input = input.Replace("\r", ""); // Normalize line endings for Windows
+
+var ranges = input.Split("\n\n")[0]
+    .Split('\n')
+    .Select(line => line.Split('-'))
+    .Select(parts => (Start: long.Parse(parts[0]), End: long.Parse(parts[1])))
+    .ToList();
+    
+var numbers = input.Split("\n\n")[1]
+    .Split('\n')
+    .Select(long.Parse)
+    .ToList();
+    
+var sortedByStart = ranges.OrderBy(r => r.Start).ToList();
+var mergedRanges = new List<(long Start, long End)>();
+
+foreach (var range in sortedByStart)
+{
+    if (mergedRanges.Count == 0 || mergedRanges.Last().End < range.Start)
+        mergedRanges.Add(range); // start new range
+    else
+    {
+        var lastRange = mergedRanges.Last();
+        mergedRanges[^1] = (lastRange.Start, Math.Max(lastRange.End, range.End)); // extend last range
+    }
+}
+
+var countInRanges = numbers.Count(number => mergedRanges.Any(r => number >= r.Start && number <= r.End));
+
+Console.WriteLine("Count of numbers within ranges:\n" + countInRanges);
